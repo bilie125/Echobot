77ca532941893ddb71968e150325c1b5cfa97aed
bilie125 <53867216+bilie125@users.noreply.github.com>
2021-06-28 23:59:51 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,28 +1,33 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-
+from imgkit.api import from_file
+from aiogram.types import input_file
 from config import TOKEN
-
-
+from bs4 import BeautifulSoup
+import requests
+import imgkit
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
-
-
-@dp.message_handler(commands=['start'])
-async def process_start_command(message: types.Message):
-    await message.reply("Привет!\nНапиши мне что-нибудь!")
-
+URL = 'https://phdpu.edu.ua/course/oo-11pm-e-12pm-f-13pm-pv/'
+page = requests.get(URL)
+  # load the page content
+text = page.content
+  
+soup = BeautifulSoup(text, "html.parser")
+with open("output.html", "w", encoding = 'utf-8') as file:
+css='style.css'
+imgkit.from_file('output.html',options=options,css=css, 'catd.jpg')
+    
+@dp.message_handler(commands=['start', 'help'])
 
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
-    await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
+    await message.reply("Напиши мне что-нибудь, и я тебе расписания")
 
-
-@dp.message_handler()
-async def echo_message(msg: types.Message):
-    await bot.send_message(msg.from_user.id, msg.text)
-
+@dp.message_handler(commands=['photo'])
+async def process_photo_command(message: types.Message):
+    await bot.send_photo(message.from_user.id,types.input_file.InputFile('catd.jpg'))
 
 if __name__ == '__main__':
     executor.start_polling(dp)
